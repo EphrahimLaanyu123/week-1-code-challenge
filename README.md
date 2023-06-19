eea531187271df6dbc2a83b583d9594fb48845e9
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-06-19 08:44:45 +0300

    Completed assighnment

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # week-1-code-challenge
+# TITLE
+Week 1 code challenge
+
+# Description
+First solution:
+The user inputs a value and the program grades it.
+
+Second solution:
+The user inputs a speed and the program checks the speed limit and gives it demerit points.
+
+Third solution:
+The program calculates the net salary of an individual.
+
+
+## Project setup
+Node engine
+Javascript
+Git clone the project and excecute by viewing the browser.
+
+
+## Licence
+MIT
--- solution1.js
+++ solution1.js
@@ -1,15 +1,21 @@
-function calculateSpeed(speed){
-    const speedLimit = 70
-    const maxDemeritPoints = 12
-    const demeritPoint = Math.floor((speed - speedLimit) / 5)
+const prompt = require("prompt-sync")();
 
-    if (speed <= speedLimit ){
-        console.log("Ok")
-    }
+let a = prompt ("Enter a number:");
 
-    if (demeritPoint > maxDemeritPoints){
-        console.log("License suspended")
-    }
+console.log(a);
+var studentMarks = 40
+
+if (studentMarks < 0 || studentMarks > 100){
+    console.log("Invalid output.")
 }
-
-calculateSpeed(80);
+if (studentMarks > 79){
+    console.log("You got an A")
+} else if (studentMarks < 80 && studentMarks > 59){
+    console.log("YOu got a B")
+} else if (studentMarks < 60 && studentMarks > 48){
+    console.log("you got a C")
+}else if (studentMarks < 50 && studentMarks > 39){
+    console.log("You got a D")
+}else {
+    console.log("You got an E")
+}
